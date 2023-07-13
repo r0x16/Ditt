98783420c2190327f4e46881713b5084fb863114
Tomás Lara <tomas.lara@tnschile.com>
2023-07-13 16:40:34 +0000

    feat(cicd): Added testing environment deploy
    - Add gitlab-ci files to automate testing environment deploy
    - Parametrized and modular to add same workflows to another environments

---
+++ terraform/output.tf
@@ -0,0 +1,6 @@
+# This file indicates the output of the infrastructure created by the module
+# This values can be used by other parts of the pipeline
+
+output "ip_address" {
+  value = var.network_parameters.ip
+}
